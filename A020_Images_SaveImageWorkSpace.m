4c21fa1c8d2eeccdf751f6de287d866a3fd73f2c
Royhm <r.deleeuw@tudelft.nl>
2015-10-13 16:45:13 +0200

    Testing whether Github Works
    description….

--- A020_Images_SaveImageWorkSpace.m
+++ A020_Images_SaveImageWorkSpace.m
@@ -10,4 +10,4 @@ initval=A001_Images_Set_Experiment(expno); %define your paths and files
 initval.ImagesWorkspaceName=strcat(initval.basepath,initval.outname,'_Images',num2str(initval.maxfile),'.mat');
 [aa,ff,drift]=Get_all_data(initval);
 save(initval.ImagesWorkspaceName, 'initval','aa','ff','drift');
-disp('done');
+disp('done'); %github test 2
