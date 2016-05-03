ede65a0b23d369333001a4213ae8814a3dcf81c5
Royhm <r.deleeuw@tudelft.nl>
2016-05-03 11:52:13 +0200

    Get all data update for driftvector.txt

--- SupportingFunctions/Get_all_data.m
+++ SupportingFunctions/Get_all_data.m
@@ -2,9 +2,19 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
 %I) First, collect an image series. TIP: run this once, then save the
 %workspace including 'a3' and then comment out this section; saves lots of
-%time with repaeated runs
+%time with repeated runs
+
     lbl3=strcat(initval.basepath,initval.driftfile);
+    
+    if exist(lbl3,'file')==0
+        fileID=fopen(lbl3,'a');
+        fprintf(fileID,'0');
+        fclose(fileID);
+        drift=dlmread(lbl3);
+    elseif exist(lbl3,'file')==2
     drift=dlmread(lbl3);
+    end
+    
     lbl1=strcat(initval.basepath,initval.BFdatapath,initval.BFfiletemplate);
     aa = readtimeseries(lbl1,'tiff',[1 initval.maxfile]);              %Brightfielddata stack
     lbl2=strcat(initval.basepath,initval.FLdatapath,initval.FLfiletemplate);
