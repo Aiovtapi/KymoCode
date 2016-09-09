e8c3023600999ca7017d55f1fd6463931695742d
Royhm <r.deleeuw@tudelft.nl>
2016-09-09 17:47:26 +0200

    Gaussmlev

--- LionFit/150917V/GaussFitSimedit.m
+++ LionFit/150917V/GaussFitSimedit.m
@@ -1,4 +1,4 @@
-%% 2D Gaussian fit to spot -- Gaussplosion!!
+%% Fit to spot 
 %
 % This code is written to identify multiple spots within the cell. The
 % input are the Bacpic images, as a result of the KymoCode, of seperate 
@@ -15,7 +15,8 @@
 % sx : std in x of the spot.
 % y : y-coordinate of the spot. 
 % sy : std in y of the spot.
-%
+
+
 %% 
 
 clear all
@@ -29,10 +30,23 @@ exp='Roy_MM_Tus_dif';
 
 lionval.channr=7;
 lionval.viewchan='CFP';
-lionval.viewbac=1:30;
+lionval.viewbac=1;
 
 lionval=LionDefine(exp,lionval);
 
+%   FITTING PARAMETER INPUTS:
+%   data:       PxPxN stack of images where N is number of PxP images
+%   PSFSigma:   Microscope PSF sigma (sigma_0 for z fit, starting sigma for sigma fit)
+%   iterations: Number of iterations (default=10)
+%   fittype:    (default=1)
+%    1: XY Position, Photons, Background
+%    2: XY Position, Photons, Background, PSF Sigma
+%    3: XYZ Position, Photons, Background 
+%    4: XY Position, Photons, Background, PSF Sigma_X, PSF Sigma_Y
+
+PSFSigma=1;
+iterations=10;
+fittype=4;
     
 for Cell=lionval.viewbac;
     
@@ -112,14 +126,10 @@ for Cell=lionval.viewbac;
 
         %Determine outliers for determining intensity threshold.
 
-        lowerboundchannel=7;
-
-
-        Tolerance=2;
-        sigmachange=0.9;
+        Tolerance=3;
+        sigmachange=0.2;
         how='positive';
         show=0;
-
 
         for i=1:Tsize
 
@@ -134,7 +144,9 @@ for Cell=lionval.viewbac;
         end
             ydatacrpdR1{i,1}=ydatacrpd{i};
 
-            higherboundchannel=16;
+            % Microfluidic channel boundaries (px)
+            lowerboundchannel=6;
+            higherboundchannel=18;
             
             % Intensity thresholding for outliers
             Data=ydatacrpdR1{i};
@@ -151,32 +163,44 @@ for Cell=lionval.viewbac;
                 LionSpotter(ydatacrpdR1{i,j},SA,Sx,Sy,Px,Py,Bs,lob,upb);
             
            %still needs background level to be mediated from within the channel.   
-            while x0{j}(i,1)>IntensityPeakThreshold && j<10 && Size{i,j}(1)>0
+           
+            while x0{j}(i,1)>IntensityPeakThreshold && j<4 && Size{i,j}(1)>0
 
-            % make guesses for spot position
-
-            % output = [guessvector,case,newcutoutimage,imageusedforfitting]
-            % guessvector = [ampl,x,sigx,y,sigy]
-            % case = 2 (spot out of channel), -1 (left edge), 0 (middle), 1 (right
-            % edge)
-            % imageusedforfitting = only the spot is used for fitting (cut out)
-
-            % preparing framework for gaussian fitting
+            Fitdata=mat2im(Ydata{i,j});
             
-            [Ydata_X(j),Ydata_Y(j)]=size(Ydata{i,j});
-            [X{j},Y{j}] =  meshgrid(linspace(1,Ydata_Y(j),Ydata_Y(j)), ...
-                linspace(1,Ydata_X(j),Ydata_X(j)));
-            xdata{1,j} = X{j};
-            xdata{2,j} = Y{j};
-
-            % define upper boundary (Size{i,j}(2) is the X size of the crpd-image)
-            ub = [realmax('double'),Size{i,j}(2),(Size{i,j}(2))^2,Size{i,j}(2), ...
-                (Size{i,j}(2))^2]; %upper bounds
+            % Main Fitting Algorithm
+            
+            [P,CRLB,LL,t]=gaussmlev2(Fitdata,PSFSigma,iterations,fittype);
+            
+%       OUTPUTS:
+%   P:      Found parameter values by fittype:
+%    1: [X Y Photons BG] 
+%    2: [X Y Photons BG Sigma] 
+%    3: [X Y Photons BG Z]  
+%    4: [X Y Photons BG SigmaX SigmaY] 
+%   CRLB:   Cramer-Rao lower bound calculated using P
+%   LL:     Log-Likelihood calculated using Stirling's approximation      
+%   t:      Execution time of fitting code. 
+%
+            
+            P(1)=P(1)+1; %dip-image has origin (0,0)
+            P(2)=P(2)+1;
+            
+            % transform to own format
+            x{j}(i,1)=P(3); 
+            x{j}(i,2)=P(1); 
+            x{j}(i,3)=P(5); 
+            x{j}(i,4)=P(2);
+            x{j}(i,5)=P(6);
+            
+%             % define upper boundary (Size{i,j}(2) is the X size of the crpd-image)
+%             ub = [realmax('double'),Size{i,j}(2),(Size{i,j}(2))^2,Size{i,j}(2), ...
+%                 (Size{i,j}(2))^2]; %upper bounds
 
             % Do the Gaussian fitting
 
-            [x{j}(i,:),resnorm,residual,exitflag] = lsqcurvefit(@GaussPlosFunc, ...
-               x0{j}(i,:),xdata(:,j),Ydata{i,j},lb,ub,OPTIONS);
+%             [x{j}(i,:),resnorm,residual,exitflag] = lsqcurvefit(@GaussPlosFunc, ...
+%                x0{j}(i,:),xdata(:,j),Ydata{i,j},lb,ub,OPTIONS);
 
            % X-SA-1 is the length in X to the point X-SA on the image.
 
@@ -240,7 +264,7 @@ for Cell=lionval.viewbac;
 
             XNorm{j}(i,2)=x{j}(i,2)/Size{i,j}(2);
 
-            XNorm{j}(i,4)=(x{j}(i,4))/Size{i,j}(1);
+            XNorm{j}(i,4)=x{j}(i,4)/Size{i,j}(1);
 
             GaussmaskW=GaussFactor*(x{j}(i,3).^2+x{j}(i,5).^2)^(1/2);
             ClipmaskR=ClipFactor*(x{j}(i,3).^2+x{j}(i,5).^2)^(1/2);
@@ -286,276 +310,23 @@ for Cell=lionval.viewbac;
     end
 
 
-    %% Translate back to original coords
+    %% plot
 
-    % use XNorm for normalization to position w.r.t. cell
-
-    %% Integrated Intensity V2.0
-    II=cell(5,5,Tsize);
-    FCII=cell(1,Tsize);
-    ROI=cell(Tsize,Nspots);
-    Ispot=zeros(Tsize,Nspots);
-
-    %SNR vars
-    SNR=zeros(Tsize,Nspots);
-    SNRSimonetti=zeros(Tsize,Nspots);
-    SNRBlur=zeros(Tsize,Nspots); %theoretical SNR of noise simulator
-
-    %BlurNoise Parameters
-    MeanNoise=1500;
-    STDNoise=225;
-
-    Gain=1;
-
-
-    for i=1:Tsize
-
-         % Full Cell Integrated Intensity
-         FCII{i}=ydatacrpd{i}(lowerboundchannel:higherboundchannel,:);
-
-        for j=1:NSpots
-
-            %This is the full cell integrated intensity
-            x{j}(i,7)=sum(sum(FCII{i}));
-            % to do: compare spot positions
-            if ~isempty(Size{i,j}) % there still has to be an image.
-
-            padx=5;
-            pady=5;
-
-            [r,c]=size(ydatacrpdR1{i,j});
-            [xx_ori,yy_ori]=meshgrid(-5:5,-5:5);
-
-
-            GaussmaskW=GaussFactor*(x{j}(i,3).^2+x{j}(i,5).^2)^(1/2);
-            ClipmaskR=ClipFactor*(x{j}(i,3).^2+x{j}(i,5).^2)^(1/2);
-
-            ydatacrpdR2{i,j}=[zeros(r,padx) ydatacrpdR1{i,j} zeros(r,padx)]; %make pads because spots can be on the edge of the image
-            ydatacrpdR2{i,j}=[zeros(pady,size(ydatacrpdR2{i,j},2)); ...
-            ydatacrpdR2{i,j};zeros(pady,size(ydatacrpdR2{i,j},2))];
-            [R,C]=size(ydatacrpdR2{i,j});
-            [XX,YY]=meshgrid(1:C,1:R);
-
-    %         if x{j}(i,2)>3 && x{j}(i,2)<Size{i,j}(2)-3
-    %         xx0=x{j}(i,2)+xx_ori;
-    %         elseif x{j}(i,2)>Size{i,j}(2)-3 && Size{i,j}(2)>1
-    %         xx0=Size{i,j}(2)-3+xx_ori;
-    %         else
-    %         xx0=xx_ori+3;
-    %         end
-
-            xx0=x{j}(i,2)+padx+xx_ori;
-            yy0=x{j}(i,4)+pady+yy_ori;
-
-            ROI{i,j}=interp2(XX,YY,ydatacrpdR2{i,j},xx0,yy0);
-
-            [rROI,cROI]=size(ROI{i,j});
-
-            xROI=cROI/2;
-            yROI=rROI/2;
-
-            [~,~,Ispot(i,j),Ibackground_level(i,j),spotim_masked,bckim]=DoubleMaskedCom(ROI{i,j},xROI...
-                ,yROI,ClipmaskR,GaussmaskW);
-
-            RadiusSpot(i,j)=GaussmaskW;
-
-            nn=size(spotim_masked(spotim_masked>0));
-
-            BCKIM=bckim(1:5,:);
-
-            SNR(i,j)=mean(spotim_masked(spotim_masked>0))/std(BCKIM(:));
-
-            %SNRSimonetti(i,j)=(sum(spotim_masked(spotim_masked>0))-nn(1)*Ibackground_level)...
-            %    /sqrt((mean(spotim_masked(spotim_masked>0))-nn(1)*Ibackground_level)/Gain)+nn(1)*std(bckim(:))^2+(nn(1)*std(bckim(:))^2)/size(bckim(:),1);
-
-            %SNRBlur(i,j)=(sum(spotim_masked(spotim_masked>0))-nn(1)*MeanNoise)...
-            %    /sqrt((mean(spotim_masked(spotim_masked>0))--nn(1)*MeanNoise)/Gain)+nn(1)*STDNoise^2+(nn(1)*STDNoise^2)/size(bckim(:),1);
-
-            else 
-
-            ROI{i,j}=0; SNR(i,j)=0;
-            nwx{i,j}=0; nwy{i,j}=0; Ispot(i,j)=0; Ibackground_level(i,j)=0;
-            spotim_masked=0;
-            bckim=0;
-
-            end
-
-            x{j}(i,8)=Ispot(i,j);
-
+    for i=1
+        for j=1
+    
+    figure(1)
+    hold on
+    imagesc(ydatacrpd{i,j})
+    plot(x{j}(i,2),x{j}(i,4),'r+')
+    hold off
+    axis([0 Size{i,j}(2)+1 0 size(ydatacrpd{i},1)+1])
+    
         end
     end
 
-    %% Integrated Intensity Calculation
-
-
-    % for i=1:Zsize
-    %     
-    %     % Full Cell Integrated Intensity
-    %     FCII{i}=ydatacrpd{i}(lob:upb,1:Size{i,j}(2));
-    %     
-    %     for j=1:Nspots
-    %     
-    %     %This is the full cell integrated intensity
-    %     x{j}(i,7)=sum(sum(FCII{i}));
-    %     
-    %         % define bordercases
-    %         % when x it is too close to the left border of image
-    %         if round(x{j}(i,2))<=1
-    %         BorderCase=-1;
-    %         % when x is too close to the right border of image
-    %         elseif round(x{j}(i,2))>=Size{i,j}(2)-1 
-    %         BorderCase=1;
-    %         else 
-    %         BorderCase=0;
-    %         end
-    %     
-    %         
-    %         %define bordercases for wider spot
-    %         if round(x{j}(i,2))<=3
-    %         BorderCaseWide=-1;
-    %         % when x is too close to the right border of image
-    %         elseif round(x{j}(i,2))>=Size{i,j}(2)-3
-    %         BorderCaseWide=1;
-    %         else 
-    %         BorderCaseWide=0;
-    %         end
-    %     % SPOT around centroid
-    %     % If sigmaX == 2 or sigmaX == 1
-    %     
-    %     %For BorderCase == 0
-    %     if (round(x{j}(i,3))==2 || round(x{j}(i,3))==1) && BorderCase ==0;
-    %     
-    %         for n=[-1 0 1] 
-    %             if round(x{j}(i,5))==2 || round(x{j}(i,5))==1
-    %                 for k=[-1 0 1]
-    %                     II{n+2,k+2,i}=ydatacrpd{i}(round(x{j}(i,4))+k,round(x{j}(i,2))+n);
-    %                 end
-    %             end
-    %             if round(x{j}(i,5))==3 || round(x{j}(i,5))==4
-    %                 for k=[-2 -1 0 1 2]
-    %                     II{n+2,k+3,i}=ydatacrpd{i}(round(x{j}(i,4))+k,round(x{j}(i,2))+n);
-    %                 end
-    %             end
-    %         end
-    %     
-    %     % For BorderCase == -1
-    %     elseif (round(x{j}(i,3))==2 || round(x{j}(i,3))==1) && BorderCase == -1;
-    %         
-    %         for n=[-1 0 1] 
-    %             if round(x{j}(i,5))==2 || round(x{j}(i,5))==1
-    %                 for k=[-1 0 1]
-    %                     II{n+2,k+2,i}=ydatacrpd{i}(round(x{j}(i,4))+k,2+n); %2 indicates minimum distance
-    %                 end                                                     %from left border                                    
-    %             elseif round(x{j}(i,5))==3 || round(x{j}(i,5))==4
-    %                 for k=[-2 -1 0 1 2]
-    %                     II{n+2,k+3,i}=ydatacrpd{i}(round(x(i,4))+k,2+n); 
-    %                 end
-    %             end
-    %         end
-    % 
-    %     %For BorderCase == 1
-    %     elseif (round(x{j}(i,3))==2 || round(x{j}(i,3))==1) && BorderCase == 1;
-    %         
-    %         for n=[-1 0 1] 
-    %             if round(x{j}(i,5))==2 || round(x{j}(i,5))==1
-    %                 for k=[-1 0 1]
-    %                     II{n+2,k+2,i}=ydatacrpd{i}(round(x{j}(i,4))+k,Size{i,j}(2)-2+n); 
-    % 
-    %                 end                                                                                         
-    %             elseif round(x{j}(i,5))==3 || round(x{j}(i,5))==4
-    %                 for k=[-2 -1 0 1 2]
-    %                     II{n+2,k+3,i}=ydatacrpd{i}(round(x{j}(i,4))+k,Size{i,j}(2)-2+n); 
-    %                 end
-    %             end
-    %         end
-    %     
-    %     % For sigmaX==3 or sigmaX==4 and Bordercase == 0
-    %     elseif (round(x{j}(i,3))==3 || round(x{j}(i,3))==4) && BorderCaseWide == 0;
-    %         
-    %         for n=[-2 -1 0 1 2] 
-    %             if round(x{j}(i,5))==2 || round(x{j}(i,5))==1
-    %                 for k=[-1 0 1]
-    %                     II{n+3,k+2,i}=ydatacrpd{i}(round(x{j}(i,4))+k,round(x{j}(i,2))+n);
-    %                 end
-    %             elseif round(x{j}(i,5))==3 || round(x{j}(i,5))==4
-    %                 for k=[-2 -1 0 1 2]
-    %                     II{n+3,k+3,i}=ydatacrpd{i}(round(x{j}(i,4))+k,round(x{j}(i,2))+n);
-    %                 end
-    %             end
-    %         end
-    %         
-    %     elseif (round(x{j}(i,3))==3 || round(x{j}(i,3))==4) && BorderCaseWide == -1;
-    %         
-    %         for n=[-2 -1 0 1 2] 
-    %             if round(x{j}(i,5))==2 || round(x{j}(i,5))==1
-    %                 for k=[-1 0 1]
-    %                     II{n+3,k+2,i}=ydatacrpd{i}(round(x{j}(i,4))+k,3+n);
-    %                 end
-    %             elseif round(x{j}(i,5))==3 || round(x{j}(i,5))==4
-    %                 for k=[-2 -1 0 1 2]
-    %                     II{n+3,k+3,i}=ydatacrpd{i}(round(x{j}(i,4))+k,3+n);
-    %                 end
-    %             end
-    %         end
-    %         
-    %     elseif (round(x{j}(i,3))==3 || round(x{j}(i,3))==4) && BorderCaseWide == 1;
-    %         
-    %         for n=[-2 -1 0 1 2] 
-    %             if round(x{j}(i,5))==2 || round(x{j}(i,5))==1
-    %                 for k=[-1 0 1]
-    %                     II{n+3,k+2,i}=ydatacrpd{i}(round(x{j}(i,4))+k,Size{i,j}(2)-3+n);
-    %                 end
-    %             elseif round(x{j}(i,5))==3 || round(x{j}(i,5))==4
-    %                 for k=[-2 -1 0 1 2]
-    %                     II{n+3,k+3,i}=ydatacrpd{i}(round(x{j}(i,4))+k,Size{i,j}(2)-3+n);
-    %                 end
-    %             end
-    %         end
-    %     end
-    %     
-    %         SII=[II{:,:,i}];
-    %         x{j}(i,6)=sum(SII(:));
-    %         XNorm{j}(i,6)=sum(SII(:));
-    %     end
-    % end
-
-
-
-    %% plot
-
-    % for i=1
-    %     for j=1
-    % 
-    % figure(1)
-    % hold on
-    % imagesc(ydatacrpdR1{i,j})
-    % plot(x{j}(i,2),x{j}(i,4),'r+')
-    % hold off
-    % axis([0 Size{i,j}(2)+1 0 size(ydatacrpd{i},1)+1])
-    % 
-    % xdatafit = linspace(-Size{i,j}(2),Size{i,j}(2)*2,300);
-    % hdatafit = x{j}(i,1)*exp(-(xdatafit-x{j}(i,2)).^2/(2*x{j}(i,3)^2));
-    % vdatafit = x{j}(i,1)*exp(-(xdatafit-x{j}(i,4)).^2/(2*x{j}(i,5)^2));
-    % 
-    % 
-    % figure(2)
-    % hold on
-    % plot(ydatacrpdR1{i,j}(round(x{j}(i,4)),:),'ob');
-    % plot(ydatacrpdR1{i,j}(:,round(x{j}(i,2))),'or');
-    % plot(xdatafit,hdatafit,'-b',xdatafit,vdatafit,'-r')
-    % axis([-5 30 0 1000])
-    % hold off
-    % legend('Horizontal','Vertical')
-    %     end
-    % end
-
     %% Save results
-    %   save(strcat(Mainfolder,'DataMULTI/',num2str(Cell)),'x','XNorm','NSpots','SNR','ydatacrpd');
-
-    % if ~exist(strcat(lionval.bacfolder,'Results'))
-    %     mkdir(strcat(lionval.bacfolder,'Results'));
-    % end
-
+    
             display(strcat('Cell ',num2str(Cell),' analyzed'));
             display('Saving..');
         if ~exist(strcat(lionval.Mainfolder,'Results'),'dir')
@@ -565,8 +336,9 @@ for Cell=lionval.viewbac;
         if exist(strcat(lionval.Mainfolder,'Results',lionval.OSslash,thisbacfolder,'.mat'))
             disp('This bac series has already been saved. Saving will be skipped')
         else
-            save(strcat(lionval.Mainfolder,'Results',lionval.OSslash,thisbacfolder),'x','XNorm','NSpots','SNR','ydatacrpdR1','pixels','imgflipped');
-            display('Save complete.');
+            save(strcat(lionval.Mainfolder,'Results',lionval.OSslash,thisbacfolder),'x','XNorm','NSpots','ydatacrpd','pixels','imgflipped');
+            display('Save complete in: ');
+            display(strcat(lionval.Mainfolder,'Results',lionval.OSslash,thisbacfolder));
         end
 
 
